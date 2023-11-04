c516797ec8ed977021e098cccb61ad43ce024e45
deepakkumar1097 <itisdeepakkumar@hotmail.com>
2023-11-04 08:19:47 +0530

    initial commit

---
+++ src/app/components/Login.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="login-container max-w-2xl px-8 py-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+      <form action="">
+        <div className="login-username">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Enter Username"
+          />
+        </div>
+        <div className="login-password mt-5 flex flex-col">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Password
+          </label>
+          <input
+            type="text"
+            name="password"
+            id="password"
+            className="block w-full rounded-md mb-4 border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Enter Password"
+          />
+          <a className=" text-sm ml-auto" href="#">
+            <span>Forgot password?</span>
+          </a>
+        </div>
+        <div className="login-button mt-5">
+          <button className=" w-full text-white linear rounded-md bg-black px-4 py-2 text-center font-medium transition duration-200 hover:!bg-slate-800">
+            Login
+          </button>
+          <button className=" w-full mt-5 bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">
+            Create Account
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
